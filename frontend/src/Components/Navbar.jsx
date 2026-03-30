fc439b6f8a5c6eb68e8cdd5d4173b0395f6aff3f
msa-developer <shoaib9542186823@gmail.com>
2026-03-30 12:32:51 +0530

    added number of items

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,7 +1,17 @@
+import React from "react";
+import useZustand from "../zustand/zustand.js";
+
 const Navbar = () => {
+  const { images, getAllImages } = useZustand();
+
+  React.useEffect(() => {
+    getAllImages();
+  });
+
   return (
     <div className="navbar bg-primary text-primary-content rounded-2xl">
       <button className="btn btn-ghost text-xl">Upload</button>
+      images:{images.length}
     </div>
   );
 };
