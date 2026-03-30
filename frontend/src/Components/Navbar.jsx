f0ce58d09b56d6de0c5098a66def37666961bfea
msa-developer <shoaib9542186823@gmail.com>
2026-03-30 11:57:37 +0530

    added border to navbar

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <div className="navbar bg-primary text-primary-content">
+    <div className="navbar bg-primary text-primary-content rounded-2xl">
       <button className="btn btn-ghost text-xl">Upload</button>
     </div>
   );
