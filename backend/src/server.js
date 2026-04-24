1ff83f2aab3e66e1050f2e76860b0c56586cd590
msa-developer <shoaib9542186823@gmail.com>
2026-04-24 21:25:57 +0530

    making ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,10 +3,15 @@ import express from "express";
 import connectDB from "./config/db.js";
 import imgRouter from "./routes/img.route.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config({ quiet: true });
 
 const app = express();
+
+app.use(express.json());
+
+const __dirname = path.resolve();
 
 app.use(
   cors({
@@ -14,9 +19,14 @@ app.use(
   }),
 );
 
-app.use(express.json());
-
 app.use("/api", imgRouter);
+
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.get("*", (_, res) =>
+    res.send(path.join(__dirname, "../frontend", "dist", "index.html")),
+  );
+}
 
 connectDB().then(() => {
   app.listen(process.env.PORT, () => {
