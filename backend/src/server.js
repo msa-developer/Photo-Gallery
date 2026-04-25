393ba59daccec8d46c672ac18a26e6b2cb84e386
msa-developer <shoaib9542186823@gmail.com>
2026-04-25 07:39:36 +0530

    changed the version of express from 5 to 4

--- backend/src/server.js
+++ backend/src/server.js
@@ -25,7 +25,7 @@ app.use("/api", imgRouter);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/{*splat}", (_, res) =>
+  app.get(/.*/, (_, res) =>
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html")),
   );
 }
